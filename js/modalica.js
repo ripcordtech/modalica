d1692edef1a4a00567d311d07cabe7acbe382015
Romain Billard <billard.rom@gmail.com>
2014-12-04 00:04:36 +0100

    Modalica
    Modalica is a very simple modal-box jQuery plugin.

---
+++ js/modalica.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$.fn.modalica = function(){
+		var modalica = $.fn.modalica;
+		var body = $('body'); 
+
+		modalica.init = function() {
+			body.append('<div class="modalica-modal"><div class="modalica--wrapper"><div class="modalica--close"><span>x</span></div><div class="modalica--content"></div></div></div>');
+		};
+		modalica.init();
+
+		var modalica_modal = $('.modalica-modal');
+		var modalica_content = modalica_modal.find('.modalica--content');
+
+		this.click(function(event){
+			event.preventDefault();
+			var content_id = $(this).attr('href');
+			modalica.open(content_id);
+		});
+
+		$('.modalica--close').click(function(event){
+			event.preventDefault();
+			modalica.close();
+		});
+
+		modalica.open = function( content_id ) {
+			body.addClass('modalica-active');
+			var html = $(content_id).html();
+			modalica_content.empty().append(html);
+			modalica_modal.addClass('active');
+			
+		};
+
+		modalica.close = function() {
+			modalica_modal.removeClass('active'); 
+			body.removeClass('modalica-active');
+		};
+
+	};
+});
