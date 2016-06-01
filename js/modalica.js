10d788b5bae5ee63d8369c6125694eb6aba5a399
ripcordtech <ryan@ripcordtech.ca>
2016-06-01 10:24:12 -0400

    Close modal with browser Back button
    Uses the html5 dom for browser history to listen for a popstate event and close the modal if a modal is open
    Requires pushing into history when the modal is open so that the popstate event doesn't leave the page
    There's a bug now where the history fills up with modal navigations, meaning the back button would have to be pressed multiple times to leave the page after a modal has been opened 1 or more times. A solution is proposed in comments, but fails in testing.

--- js/modalica.js
+++ js/modalica.js
@@ -7,6 +7,15 @@ $(document).ready(function() {
 			if ($('.modalica-modal').length == 0) { //Check that body.append hasn't been called already for a previous init
 				body.append('<div class="modalica-modal"><div class="modalica--wrapper"><div class="modalica--close"><span>x</span></div><div class="modalica--content"></div></div></div>');
 			}
+			//Support pressing back button to close modal:
+			if (window.history && window.history.pushState) { //does browser support?
+	                	$(window).on('popstate', function() { //attach to history event
+                			if ($('.modalica-active').length > 0) { //check modal is open
+		                        	$(".modalica--close")[0].click(); //close modal
+		                        	return false; //override browser event
+		                    	}
+		                });
+			}
 		};
 		modalica.init();
 
@@ -25,6 +34,9 @@ $(document).ready(function() {
 		});
 
 		modalica.open = function( content_id ) {
+			if (window.history) {
+				window.history.pushState(null, null, content_id); //Add to browser history
+			}
 			body.addClass('modalica-active');
 			var html = $(content_id).html();
 			modalica_content.empty().append(html);
@@ -35,6 +47,12 @@ $(document).ready(function() {
 		modalica.close = function() {
 			modalica_modal.removeClass('active'); 
 			body.removeClass('modalica-active');
+			//Pushes history back when the modal is closed using modal close button
+			//Leave commented out as a bug exists where the browser so go back too far
+			//Causing the user to leave the page when closing the modal
+			//if (window.history && !window.location.hash == '') {
+                	//	window.history.back();
+            		//}
 		};
 
 	};
